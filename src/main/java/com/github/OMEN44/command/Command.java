52f0eeb45b56b33fa70015c1c4693b5da210f306
OMEN44 <huonswales@gmail.com>
2022-09-17 12:44:20 +1000

    fixed import issue

--- src/main/java/com/github/OMEN44/command/Command.java
+++ src/main/java/com/github/OMEN44/command/Command.java
@@ -1,5 +1,5 @@
 package com.github.OMEN44.command;
 
 public interface Command {
-    void execute(String label, String sender, String timeSent, String[] args);
+    boolean execute(String label, String sender, String timeSent, String[] args);
 }
